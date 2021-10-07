2822690db53e58f103dff8cff01f72fa60216922
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-10-07 00:46:05 -0400

    CREATED index file in scrapers folder to import/export all scrapers

---
+++ scrapers/_index.mjs
@@ -0,0 +1,7 @@
+import typingSpeedTestMjs from "./typing-speed-test.mjs"
+import autoLoginQuotesToScrapeMjs from "./auto-login-quotesToScrape.mjs"
+
+export {
+    typingSpeedTestMjs,
+    autoLoginQuotesToScrapeMjs
+}
